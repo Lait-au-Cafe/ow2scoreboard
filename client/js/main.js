1c12b104e31077492c956bfa874104b9034629b4
Lait-au-Cafe <laitaucafe8@gmail.com>
2023-01-18 03:23:00 +0900

    Improve the visibility of current rank in client view.

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,13 @@
 'use strict'
 
 {
+    setInterval(() => {
+        console.log("Flip")
+        $('.badge').each((index, elem) => {
+            $(elem).toggleClass('flipped');
+        });
+    }, 10000);
+
     function update_score() {
         (async () => {
             const response = await fetch("http://localhost:3000/api/get_score", {
@@ -12,13 +19,21 @@
                 roles.forEach((role, index) => {
                     const row = $(`tr.${role}`)
                     const badge = $(`.${role} .badge img`)
+                    const division = $(`.${role} .badge span`)
                     const win = $(`.${role} .win`)
                     const loss = $(`.${role} .loss`)
                     const draw = $(`.${role} .draw`)
-
+                    
                     if(!score_data[index].is_visible) { row.addClass('hidden') }
                     else { row.removeClass('hidden') }
+                    
                     badge.attr('src', `../assets/rank_badge/${score_data[index].rank.current_tier}/${score_data[index].rank.current_division}.png`)
+                    
+                    let tier;
+                    if(score_data[index].rank.current_tier == 'grandmaster') { tier = 'GM' }
+                    else { tier = score_data[index].rank.current_tier.slice(0, 1).toUpperCase() }
+                    division.html(`${tier}&thinsp;${score_data[index].rank.current_division}`)
+
                     win.text(score_data[index].score.wins)
                     loss.text(score_data[index].score.losses)
                     draw.text(score_data[index].score.draws)
