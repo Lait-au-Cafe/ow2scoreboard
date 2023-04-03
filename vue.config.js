50352310c3733bd48a84ae66904442a4f375ae6e
Lait-au-Cafe <laitaucafe8@gmail.com>
2023-04-03 18:00:23 +0900

    Add icon to app. Thank you v_shiratama.

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,10 @@ module.exports = defineConfig({
   transpileDependencies: true, 
   pluginOptions: {
     electronBuilder: {
+      "build": {
+        "appId": "com.laitaucafe.ow2scoreboard",
+        "productName": "OW2Scoreboard"
+      },
       preload: 'src/preload.js', 
       customFileProtocol: './',
     }
